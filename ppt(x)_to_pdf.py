21b7146aac1f372f83c633a45d881611a38c650f
Ryan Taylor <rytay123@gmail.com>
2020-01-29 19:53:32 -0500

    Added informative output to user

--- ppt(x)_to_pdf.py
+++ ppt(x)_to_pdf.py
@@ -3,19 +3,22 @@ import subprocess
 
 files = [f for f in os.listdir('.') if ".ppt" in f or ".pptx" in f]
 
-delete = input('Delete powerpoint files?(y to delete): ')
+delete = input('Delete powerpoint files after conversion? (y to delete): ')
 
 remove = False
 
 if delete is 'y':
     remove = True
+    print('Will delete files after conversion...')
+
 
 for f in files:
     raw_proc = "unoconv -f pdf " + f
     proc = raw_proc.split()
-    print('Converting : ' + f + ' to pdf')
+    print('Converting : ' + f + ' to pdf.')
     subprocess.run(proc)
     if remove:
+        print('Deleting : ' + f + '...'
         os.remove(f)
         
 print('Done. Converted '+ len(files) + ' documents.')
